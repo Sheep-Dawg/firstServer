49acd5158de3ebf6bbe11a4ba05d5032d74d4ebd
Sheep-Dawg <bucketjensen@gmail.com>
2026-03-25 12:37:20 -0600

    fixing again

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use("/", require("./routes"));
+
+app.listen(port, () => {
+  console.log("test server running on port: ${PORT}");
+});
